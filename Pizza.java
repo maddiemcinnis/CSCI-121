43836c185b5bd6c08ba8b345b75087e235c0d051
maddiemcinnis <152915601+maddiemcinnis@users.noreply.github.com>
2024-02-14 23:21:16 -0500

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+
+public class Pizza extends JFrame implements ActionListener {
+    public static void Gui()
+    {
+        String selectedSize;
+        JRadioButton smallPizza = new JRadioButton();
+
+        smallPizza.addActionListener();
+
+        JRadioButton medium = new JRadioButton();
+        JRadioButton large = new JRadioButton();
+        JRadioButton extraLarge = new JRadioButton();
+
+
+        smallPizza.addActionListener();
+
+        JRadioButton medium = new JRadioButton();
+        JRadioButton large = new JRadioButton();
+        JRadioButton extraLarge = new JRadioButton();
+    }
+
+    public static void main(String[] args) {
+        Gui();
+    }
+}
+//
+//    JLabel question = new JLabel("What name are you placing this order under?");
+//    Font bigFont = new Font("Arial", Font.BOLD, 16);
+//    JTextField answer = new JTextField(10);
+//    JButton press = new JButton("Press");
+//    JLabel greeting = new JLabel("");
+//    final int WIDTH = 275;
+//    final int HEIGHT = 224;
+//
+
+////
+////        String name = JOptionPane.showInputDialog("Please enter the name for the order.");
+////        JOptionPane.showMessageDialog(null, "Thank you, " + name + ". You can proceed with your order.");
+//
+//    }
+//}
